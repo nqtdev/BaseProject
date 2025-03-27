348cb55acc3f8a16750afe7e24a86b5195d5dc82
nqtien <nguyenquoctien2000@gmail.com>
2025-03-27 09:20:23 +0700

    Update base

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
     screens: {
-      // Các breakpoints mặc định
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px',
-      // Thêm breakpoint xxl
       xxl: '1600px',
     },
     extend: {
